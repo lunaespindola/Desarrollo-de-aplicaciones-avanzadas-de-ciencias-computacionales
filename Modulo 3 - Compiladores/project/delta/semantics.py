0c26197e2db2be76c619b9846707e5eb9268054c
Luna Espindola <lunaespindola04@gmail.com>
2024-04-21 22:20:53 -0600

    Added tests

--- Modulo 3 - Compiladores/project/delta/semantics.py
+++ Modulo 3 - Compiladores/project/delta/semantics.py
@@ -21,9 +21,31 @@ class SemanticVisitor(PTNodeVisitor):
     def symbol_table(self):
         return self.__symbol_table
     
-    def visit_int_literal(self, node, children):
+    def visit_decimal(self, node, children):
         value = int(node.value)
-        if value < 2 ** 31:
+        if value >= 2 ** 31:
             raise SemanticMistake(
-                f'Integer overflow at line {self.position(node)} => {value}')
-        return value
+                'Out of range decimal integer literal at position '
+                f'{self.position(node)} => { value }'
+            )
+            
+    def visit_binary(self, node, children):
+        value = int(node.value[2:], 2)  # Remove '#b' prefix and convert to decimal
+        if value >= 2 ** 31:
+            raise SemanticMistake(
+                f'Out of range binary integer literal at position {self.position(node)} => {value}'
+            )
+
+    def visit_octal(self, node, children):
+        value = int(node.value[2:], 8)  # Remove '#o' prefix and convert to decimal
+        if value >= 2 ** 31:
+            raise SemanticMistake(
+                f'Out of range octal integer literal at position {self.position(node)} => {value}'
+            )
+
+    def visit_hexadecimal(self, node, children):
+        value = int(node.value[2:], 16)  # Remove '#x' prefix and convert to decimal
+        if value >= 2 ** 31:
+            raise SemanticMistake(
+                f'Out of range hexadecimal integer literal at position {self.position(node)} => {value}'
+            )
