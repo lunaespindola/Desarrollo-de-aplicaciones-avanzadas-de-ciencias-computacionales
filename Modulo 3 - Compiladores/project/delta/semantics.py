4e3653e227dd878145460a025740729982cfee8a
Luna Espindola <lunaespindola04@gmail.com>
2024-04-22 17:55:42 -0600

    Added to current code

--- Modulo 3 - Compiladores/project/delta/semantics.py
+++ Modulo 3 - Compiladores/project/delta/semantics.py
@@ -9,6 +9,9 @@ class SemanticMistake(Exception):
 
 class SemanticVisitor(PTNodeVisitor):
 
+    RESERVED_WORDS = ['true', 'false', 'var',
+                      'if', 'else', 'while']
+
     def __init__(self, parser, **kwargs):
         super().__init__(**kwargs)
         self.__parser = parser
@@ -20,7 +23,7 @@ class SemanticVisitor(PTNodeVisitor):
     @property
     def symbol_table(self):
         return self.__symbol_table
-    
+
     def visit_decimal(self, node, children):
         value = int(node.value)
         if value >= 2 ** 31:
@@ -28,23 +31,53 @@ class SemanticVisitor(PTNodeVisitor):
                 'Out of range decimal integer literal at position '
                 f'{self.position(node)} => { value }'
             )
-            
+
+    def visit_decl_variable(self, node, children):
+        name = node.value
+        if name in SemanticVisitor.RESERVED_WORDS:
+            raise SemanticMistake(
+                'Reserved word not allowed as variable name at position '
+                f'{self.position(node)} => {name}'
+            )
+        if name in self.__symbol_table:
+            raise SemanticMistake(
+                'Duplicate variable declaration at position '
+                f'{self.position(node)} => {name}'
+            )
+        self.__symbol_table.append(name)
+
+    def visit_lhs_variable(self, node, children):
+        name = node.value
+        if name not in self.__symbol_table:
+            raise SemanticMistake(
+                'Assignment to undeclared variable at position '
+                f'{self.position(node)} => {name}'
+            )
+
+    def visit_rhs_variable(self, node, children):
+        name = node.value
+        if name not in self.__symbol_table:
+            raise SemanticMistake(
+                'Undeclared variable reference at position '
+                f'{self.position(node)} => {name}'
+            )
+
     def visit_binary(self, node, children):
-        value = int(node.value[2:], 2)  # Remove '#b' prefix and convert to decimal
+        value = int(node.value[2:], 2) 
         if value >= 2 ** 31:
             raise SemanticMistake(
                 f'Out of range binary integer literal at position {self.position(node)} => {value}'
             )
 
     def visit_octal(self, node, children):
-        value = int(node.value[2:], 8)  # Remove '#o' prefix and convert to decimal
+        value = int(node.value[2:], 8) 
         if value >= 2 ** 31:
             raise SemanticMistake(
                 f'Out of range octal integer literal at position {self.position(node)} => {value}'
             )
 
     def visit_hexadecimal(self, node, children):
-        value = int(node.value[2:], 16)  # Remove '#x' prefix and convert to decimal
+        value = int(node.value[2:], 16)  
         if value >= 2 ** 31:
             raise SemanticMistake(
                 f'Out of range hexadecimal integer literal at position {self.position(node)} => {value}'
